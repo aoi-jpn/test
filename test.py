2245ebb874721078191d9191856f402e2711901a
x.aoi.k.x@gmail.com <x.aoi.k.x@gmail.com>
2021-08-07 20:35:46 +0900

    test

---
+++ test.py
@@ -0,0 +1,16 @@
+def pop(num):
+    print("こんにちは")
+    a=10
+    b=9
+    print(a+b)
+    if a+b>20:
+        print("20より大きい")
+    else:
+        print("20より小さい")
+    print("テスト")
+    for i in range(1,num):
+        print(i)
+    testlist=["犬","猫","とり","うさぎ","猿"]
+    for i in testlist:
+        print(i)
+pop(100)
